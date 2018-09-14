cc830d0fe97219d1ae543ed7c8e0fb7836577d47
mahuiying <mahuiying@koolearn-inc.con>
2018-09-14 10:17:02 +0800

    first commit

---
+++ MY_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MY_Demo
+//
+//  Created by 马慧莹 on 2018/9/14.
+//  Copyright © 2018年 magic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
